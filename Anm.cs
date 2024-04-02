e045597318de99cf78730ecc7f288c78d61d2e46
MichaelHinrichs <mchinrichs@gmail.com>
2024-04-02 16:36:28 -0500

    Initial commit

---
+++ Anm.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Emerland_Solitaire_Endless_Journey_anm
+{
+    public static class Anm
+    {
+        static StreamReader sr;
+        public static float[][] Read(string[] args)
+        {
+            sr = new StreamReader(File.OpenRead(args[0]));
+            sr.Read();
+            int lines = int.Parse(sr.ReadLine());
+            sr.ReadLine();
+            float[][] frames = new float[lines][];
+            for (int i = 0; i < lines; i++)
+                frames[i] = Floats();
+
+            return frames;
+        }
+
+        public static float[] Floats()
+        {
+            string[] floatstrings = sr.ReadLine().Split(' ');
+            float[] floats = new float[6];
+            for (int i = 0; i < 6; i++)
+                floats[i] = float.Parse(floatstrings[i]);
+
+            return floats; 
+        }
+    }
+}
